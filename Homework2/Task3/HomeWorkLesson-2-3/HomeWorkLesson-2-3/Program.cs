8281712a42b5bb80098676c1bfd04bffdeb79950
Irina-Puh <1muha@mail.ru>
2021-01-24 22:07:09 +0300

    added third task to lesson2

---
+++ Homework2/Task3/HomeWorkLesson-2-3/HomeWorkLesson-2-3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HomeWorkLesson_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите любое целое число");
+            int dig = Convert.ToInt32(Console.ReadLine());
+            if ((dig % 2) == 0)
+            {
+                Console.WriteLine("Вы ввели четное число");
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели нечетное число");
+            }
+        }
+    }
+}
