f55c52ef9aa3f065791e0bcb288f29f33a82bd30
Irina-Puh <1muha@mail.ru>
2021-02-01 22:41:40 +0300

    added solutions to Home Task Lesson 4

---
+++ HomeWork4/HomeWorkLesson-4-1/HomeWorkLesson-4-1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HomeWorkLesson_4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Write("Введите Фамилию:  ");
+                string last_Name = Console.ReadLine();
+                Console.Write("Введите Имя:  ");
+                string first_Name = Console.ReadLine();
+                Console.Write("Введите Отчество:  ");
+                string patronimic_Name = Console.ReadLine();
+                Console.WriteLine();
+                string full_Name = GetFullName(last_Name, first_Name, patronimic_Name);
+                Console.WriteLine($"Вы ввели: {full_Name}");
+
+                Console.WriteLine($"Хотите ввести еще? \n Нажмите любую клавишу для продолжнения. Для завершения-нажмите<Esc> ");
+                
+            } while (Console.ReadKey().Key!=ConsoleKey.Escape);
+
+                        
+        }
+
+        static string GetFullName(string lastName, string firstName, string patronimic)
+        {
+
+            string fullName = lastName + ' ' + firstName + ' ' + patronimic;
+            return fullName;
+        }
+    }
+}
---
+++ HomeWork4/HomeWorkLesson-4-3/HomeWorkLesson-4-3/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace HomeWorkLesson_4_3
+{
+    class Program
+    {
+        enum Seasons
+        {
+            Winter = 1,
+            Spring,
+            Summer,
+            Autumn
+        }
+
+        static void Main(string[] args)
+        {
+            int monthNr;
+            do {
+                Console.Write("Введите номер текущего месяца: ");
+                monthNr = Convert.ToInt32(Console.ReadLine());
+
+                if (monthNr > 12)
+                    Console.WriteLine("Ошибка: введите число от 1 до 12");
+            }
+            while (monthNr > 12);
+
+            int seasonNr = FindSeason(monthNr);
+            string nameSeason = GetNameSeason(seasonNr);
+            Console.WriteLine($"Сейчас {nameSeason}");
+         
+        }
+        
+        static int FindSeason (int nrMonth)
+        {
+            int nrSeason=0;
+            switch (nrMonth)
+            {
+                case 1:
+                case 2:
+                case 12:
+                    nrSeason = (int)Seasons.Winter;
+                    break;
+                case 3:
+                case 4:
+                case 5: 
+                    nrSeason = (int)Seasons.Spring;
+                    break;
+                case 6:
+                case 7:
+                case 8: 
+                    nrSeason = (int)Seasons.Summer;
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    nrSeason = (int)Seasons.Summer;
+                    break;
+                    
+            }
+            return nrSeason;
+        }
+
+        static string GetNameSeason(int nomerSeason)
+        {
+            string seasonName = "";
+            switch (nomerSeason)
+            {
+                case 1:
+                     seasonName =  "Зима";
+                        break;
+                case 2:
+                    seasonName = "Весна";
+                    break;
+                case 3:
+                    seasonName = "Лето";
+                    break;
+                case 4:
+                    seasonName = "Осень";
+                    break;
+            }
+            return seasonName;
+        }
+    }
+}
