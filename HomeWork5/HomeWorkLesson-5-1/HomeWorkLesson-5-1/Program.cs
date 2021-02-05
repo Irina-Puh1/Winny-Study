628ebdb34c9c319dcd222fc9e6731b7a2efdcf75
Irina-Puh <1muha@mail.ru>
2021-02-05 22:35:05 +0300

    added part of tasks for 5 lesson

---
+++ HomeWork5/HomeWorkLesson-5-1/HomeWorkLesson-5-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace HomeWorkLesson_5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст для записи в файл:  ");
+            string someText = Console.ReadLine();
+            Console.Write("Введите имя файла, в который необходимо поместить текст \n (используйте латинскую раскладку клавиатуры): ");
+            string fileName = Console.ReadLine();
+            fileName = fileName + ".txt";
+            string workDir = @"D:\ExampleDir\HomeW";
+            if (Directory.Exists(workDir) == false)
+            {
+                Directory.CreateDirectory(workDir);
+            }
+
+            string fileNamePath = Path.Combine(workDir, fileName);
+           
+            File.WriteAllText(fileNamePath, someText);
+
+        }
+    }
+}
