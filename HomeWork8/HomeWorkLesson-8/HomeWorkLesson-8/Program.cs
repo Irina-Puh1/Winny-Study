4f39ac897b3a23ca70b50039849a7ebd73f0d791
Irina-Puh <1muha@mail.ru>
2021-02-15 18:27:02 +0300

    solution to the task for lesson 8

---
+++ HomeWork8/HomeWorkLesson-8/HomeWorkLesson-8/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Configuration;
+
+namespace HomeWorkLesson_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool key;
+            string greeting = "Приветствую Вас!";
+            string name;
+            string age;
+            string ocupation;
+
+            key = Check();
+            if (key)
+            {
+                name = ReadSetting("Name");
+                age = ReadSetting("Age");
+                ocupation = ReadSetting("Ocupation");
+                Console.WriteLine($"{greeting} {name}");
+                Console.WriteLine($"возраст: {age}");
+                Console.WriteLine($"профессия: {ocupation}");
+
+            }
+            else 
+            {
+                Console.WriteLine(greeting);
+                Console.Write("Введите фамилию и имя: ");
+                AddSetting("Name", Console.ReadLine());
+                Console.Write("Введите возраст: ");
+                AddSetting("Age", Console.ReadLine());
+                Console.Write("Введите профессию: ");
+                AddSetting("Ocupation", Console.ReadLine());
+            }
+                
+        }
+
+        static bool Check()
+        {
+            var Settings = ConfigurationManager.AppSettings;
+            if (Settings.Count == 0)
+                return false;
+            else
+                return true;
+        }
+
+        static string ReadSetting(string value)
+        {
+            try
+            {
+                var Setting = ConfigurationManager.AppSettings;
+
+               return Setting[value];
+                  
+            }
+            catch (ConfigurationErrorsException)
+            {
+                return "Error reading app settings";
+            }
+        }
+
+        static void AddSetting(string key, string value)
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var setting = configFile.AppSettings.Settings;
+                setting.Add(key, value);
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error writing app settings");
+            }
+        }
+    }
+}
