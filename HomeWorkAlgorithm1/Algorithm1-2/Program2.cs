961b00eb9f7567645c3ad53563e96c1482a0e547
Irina-Puh <1muha@mail.ru>
2021-02-23 16:51:40 +0300

    solution to lesson 1 of Algorithm course

---
+++ HomeWorkAlgorithm1/Algorithm1-2/Program2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Algorithm1_2
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+        public static int StrangeSum(int[] inputArray)
+        {
+            //Сложность операций присваивания от N не зависит, т.е. все операции присваивания вне циклов будут O(1)
+            //Внешний цикл повторяется N раз, Средний цикл тоже N раз, Внутреннй цикл также N раз
+            //Каждая операция присваивания внутри цикла это тоже O(1). Внутри цикла таких операций 3, т.е. 2 X O(1)
+            //Общая сложность циклов составит (O(N*3)) X  (3 X O(1)) - примем, что в большинстве случаев j!=0
+            //и общая сложность функции  - O(N*3) X  (3 X O(1)) + (2 X O(1))
+            //т.к. часть (2XO(1) + (3 X O(1))) не зависит от N, то ее можно отбросить.
+            //Таким образом сложность функции можно принять за O(N*3) (N в кубе).
+
+
+            int sum = 0; // O(1)
+            for (int i = 0; i < inputArray.Length; i++) // O(N)
+            {
+                for (int j = 0; j < inputArray.Length; j++) //O(N)
+                {
+                    for (int k = 0; k < inputArray.Length; k++) //O(N)
+                    {
+                        int y = 0; // O(1)
+
+                        if (j != 0)
+                        {
+                            y = k / j; //O(1)
+                        }
+
+                        sum += inputArray[i] + i + k + j + y;// О(1)
+                    }
+                }
+            }
+
+            return sum;
+        }
+
+    }
+}
---
+++ HomeWorkAlgorithm1/Algorithm1-3/Program3.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace Algorithm1_3
+{
+    class Program3
+    {
+        static void Main(string[] args)
+        {
+            checking();
+            Console.Write("Введите число: ");
+            int n = int.Parse(Console.ReadLine());
+            int result = FibonachiNumbersReqursion(n);
+            Console.WriteLine($"Результат вычисления с помощью рекурсии: {result}");
+            result = FibonachiNumbersCircle(n);
+            Console.WriteLine($"Результат вычисления с помощью цикла: {result}");
+
+
+        }
+
+        static void checking()
+        {
+            int[] enter_n = { 0, 1, -2, 6, 8, -1, -5, -6 };
+            int[] check_result = { 0, 1, -1, 8, 21, 1, 5, -8 };
+            for (int i = 0; i < enter_n.Length; i++)
+            {
+                int result1 = FibonachiNumbersReqursion(enter_n[i]);
+                int result2 = FibonachiNumbersCircle(enter_n[i]);
+                if (result1 == result2 && result1 == check_result[i])
+                {
+
+                    Console.WriteLine($"Число {enter_n[i]}");
+                    Console.WriteLine($"Число Фибоначчи: {result1}");
+                    Console.WriteLine("Проверка пройдена");
+
+                }
+                else if (result1 != result2 && result1 == check_result[i])
+                    Console.WriteLine($"Число: {enter_n[i]} - ошибка в методе-цикл");
+
+                else if (result1 != result2 && result2 == check_result[i])
+                    Console.WriteLine($"Число: {enter_n[i]} - ошибка в методе-рекурсия");
+                else
+                    Console.WriteLine($"Число: {enter_n[i]} - Вычисления не верны");
+            }
+
+        }
+
+
+        static int FibonachiNumbersReqursion(int k)
+        {
+            int summ;
+            if (k == 0)
+                summ = 0;
+            else if (k == 1)
+                summ = 1;
+            else
+            {
+                if (k > 0)
+                {
+                    summ = FibonachiNumbersReqursion(k - 1) + FibonachiNumbersReqursion(k - 2);
+                }
+                else
+                {
+                    summ = FibonachiNumbersReqursion(k + 2) - FibonachiNumbersReqursion(k + 1);
+                }
+            }
+            return summ;
+
+        }
+
+        static int FibonachiNumbersCircle(int k)
+        {
+            int summ = 0;
+
+            if (k == 0)
+                summ = 0;
+            else if (k == 1 || k == -1)
+                summ = 1;
+            else
+            {
+                int summ1 = 0;
+                int summ2 = 1;
+                if (k < 0)
+                {
+                    for (int i = 2; i <= k*(-1); i++)
+                    {
+                        summ = summ1 - summ2;
+                        summ1 = summ2;
+                        summ2 = summ;
+                    }
+                }
+
+                else
+                {
+                    for (int i = 2; i <= k; i++)
+                    {
+                        summ = summ1 + summ2;
+                        summ1 = summ2;
+                        summ2 = summ;
+                    }
+                }
+
+            }
+
+            return summ;
+
+        }
+
+
+    }
+}
